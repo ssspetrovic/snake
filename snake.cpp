3699b283b90a4b217e35045fece360f643073705
Srdjan Petrovic <s.petrovic852@gmail.com>
2025-01-18 02:18:45 +0100

    Encapsulate the code with a Game class, add sample image render and input handling

--- snake.cpp
+++ snake.cpp
@@ -3,29 +3,134 @@
 
 #include <SDL2/SDL.h>
 
-constexpr int WIDTH = 800;
-constexpr int HEIGHT = 600;
-
-int main(int argc, char const *argv[])
+class Game
 {
+private:
+    bool is_running = true;
+    const int WIDTH = 800;
+    const int HEIGHT = 600;
+
     SDL_Window *window = nullptr;
+    SDL_Surface *window_surface = nullptr;
+    SDL_Surface *image_surface = nullptr;
+    SDL_Event event;
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+public:
+    Game() = default;
+
+    void start()
     {
-        std::cerr << "Failed to initialize SDL video" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-    
-    window = SDL_CreateWindow("snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-    
-    if (window == NULL)
-    {
-        std::cerr << "Failed to create the window: " << SDL_GetError() << std::endl;
-        exit(EXIT_FAILURE);
+        is_running = true;
     }
 
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(5000);
+    void stop()
+    {
+        is_running = false;
+    }
 
+    void clean()
+    {
+        if (image_surface)
+        {
+            SDL_FreeSurface(image_surface);
+            image_surface = nullptr;
+        }
+
+        if (window)
+        {
+            SDL_DestroyWindow(window);
+            window = nullptr;
+        }
+
+        SDL_Quit();
+    }
+
+    ~Game()
+    {
+        clean();
+    }
+
+    bool init()
+    {
+        if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        {
+            std::cerr << "Failed to initialize SDL video" << std::endl;
+            return false;
+        }
+
+        window = SDL_CreateWindow("snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+        if (!window)
+        {
+            std::cerr << "Failed to create the window: " << SDL_GetError() << std::endl;
+            return false;
+        }
+
+        // Window is successfully created
+        window_surface = SDL_GetWindowSurface(window);
+
+        image_surface = SDL_LoadBMP("img/flower.bmp");
+        if (image_surface == NULL)
+        {
+            std::cerr << "Failed to load the image: " << SDL_GetError() << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+    void handle_key_input(SDL_KeyboardEvent key)
+    {
+        switch (key.keysym.sym)
+        {
+        case SDLK_ESCAPE:
+            stop();
+        }
+    }
+
+    void handle_events()
+    {
+        while (SDL_PollEvent(&event) != 0)
+        {
+            if (event.type == SDL_QUIT)
+            {
+                stop();
+            }
+
+            if (event.type == SDL_KEYDOWN)
+            {
+                handle_key_input(event.key);
+            }
+        }
+    }
+
+    void render()
+    {
+        SDL_BlitSurface(image_surface, NULL, window_surface, NULL);
+        SDL_UpdateWindowSurface(window);
+    }
+
+    void run()
+    {
+        while (is_running)
+        {
+            handle_events();
+            render();
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Game game;
+
+    if (!game.init())
+    {
+        std::cerr << "Failed to initialize the game." << std::endl;
+        return -1;
+    }
+
+    game.run();
+    
     return 0;
 }
